3f790e64801f5a49b3e6bc820ff6c78c7166eaeb
Jia Jun Yong <s3688090@student.rmit.edu.au>
2018-08-26 16:40:47 +1000

    oops fixed something

--- src/model/SalesStaff.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class SalesStaff extends Employee {
-
-	public SalesStaff(String username, String password) {
-		this.username = username;
-		this.password = password;
-	}
-}
--- src/model/Supplier.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Supplier extends Employee {
-
-	public Supplier(String username, String password) {
-		this.username = username;
-		this.password = password;
-	}
-}
