a1588c6fa5f221a0bdd1233a5d2ba3421d367a47
Jia Jun Yong <s3688090@student.rmit.edu.au>
2018-09-24 15:26:20 +1000

    added code that runs on exit/shutdown in main()

--- src/client/SupermarketClient.java
+++ src/client/SupermarketClient.java
@@ -8,15 +8,14 @@ import model.system.ProductManager;
 import view.WelcomeScreen;
 
 public class SupermarketClient {
-	// TEST CASES GO HERE
 
 	public static void main(String[] arg) {
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
+				ProductManager pm = new ProductManager();
+				AccountManager am = new AccountManager();
 				try {
-					ProductManager pm = new ProductManager();
-					AccountManager am = new AccountManager();
 					pm.initialiseProducts();
 					am.initialiseUsers();
 					WelcomeScreen welcomeScreen = new WelcomeScreen();
@@ -24,6 +23,15 @@ public class SupermarketClient {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+				
+				// run codes on exit
+				 Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+				        public void run() {
+				        	pm.saveProducts();
+							am.saveUsers();	
+				        }
+				    }, "Shutdown-thread"));
+				
 			}
 		});
 
